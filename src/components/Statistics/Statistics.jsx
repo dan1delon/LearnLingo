36e45bab51cebe6fc471f67de5a0b6ff50af9d06
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-09-07 11:16:01 +0300

    done home page

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,28 @@
+import css from './Statistics.module.css';
+
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <div className={css.wrapper}>
+      <ul className={css.list}>
+        <li className={css.item}>
+          <p className={css.number}>32,000 +</p>
+          <p className={css.text}>Experienced tutors</p>
+        </li>
+        <li className={css.item}>
+          <p className={css.number}>300,000 +</p>
+          <p className={css.text}>5-star tutor reviews</p>
+        </li>
+        <li className={css.item}>
+          <p className={css.number}>120 +</p>
+          <p className={css.text}>Subjects taught</p>
+        </li>
+        <li className={css.item}>
+          <p className={css.number}>200 +</p>
+          <p className={css.text}>Tutor nationalities</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Statistics;
